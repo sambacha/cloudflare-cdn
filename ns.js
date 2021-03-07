40a60ee9981ac5d558374c3d40eaf1295686c5a0
sam <sam@freighttrust.com>
2021-03-06 17:40:20 -0800

    init
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ ns.js
@@ -0,0 +1,8 @@
+module.exports = {
+  cname: {
+    '${DOMAIN_NAME}': '${DOMAIN_NAME_CNAME}',
+  },
+  txt: {
+    '${DOMAIN_NAME}': 'google-site-verification=X-${GOOGLE_SITE_VERIFICATION}',
+  },
+};
